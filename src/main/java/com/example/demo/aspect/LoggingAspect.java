a7d79266d71882873de3c2d186e132d155838688
Busra Kaya <busrakaya0697@hotmail.com>
2023-12-23 12:21:38 +0300

    Add AOP logic

--- src/main/java/com/example/demo/aspect/LoggingAspect.java
+++ src/main/java/com/example/demo/aspect/LoggingAspect.java
@@ -1,12 +1,10 @@
 package com.example.demo.aspect;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 @Aspect
@@ -25,27 +23,35 @@ public class LoggingAspect {
 //    Either keep the @Around advice, if you really think you need it.
 //    Usually, this is only the case if you want to do more than log things, e.g.
 //    modify method parameters or return values, handle exceptions or other things potentially changing the control flow:
-//    @Around("execution(* com.example.demo.service.*.*(..))")
-//    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-//        logger.info("Method called " + joinPoint.getSignature().getName());
-//        try {
-//            return joinPoint.proceed();
-//        }
-//        finally {
-//            logger.info("Method executed  " + joinPoint.getSignature().getName());
-//        }
-//    }
+    @Around("execution(* com.example.demo.service.*.*(..))")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        logger.info("Method called with name: " + joinPoint.getSignature().getName());
+        logger.info("Method args: " + Arrays.toString(joinPoint.getArgs()));
+
+        try {
+            Object returnedByMethod = joinPoint.proceed();
+            logger.info("Method returned with: " + returnedByMethod);
+            return returnedByMethod;
+//    Aspect'in ProceedingJoinPoint parametresinin proceed() metodunu çağırmazsanız,
+//    bu aspect hiçbir zaman yakalanan metoda başvurmaz.
+//    Bu durumda, aspect yalnızca yakalanan metod yerine yürütülür.
+//    Metodu çağıran, gerçek metodun hiçbir zaman yürütülmediğini bilemez.
+        }
+        finally {
+            logger.info("Method executed  " + joinPoint.getSignature().getName());
+        }
+    }
 
 //    Or keep it simple and just use a pair of @Before and @After advice methods, if there is no need to transfer data from one advice to the other.
 //    This is much simpler, because you do not need to proceed, use try-finally or return anything:
 
-    @Before("execution(* com.example.demo.service.*.*(..))")
-    public void logBefore(JoinPoint joinPoint) {
-        logger.info("[BEFORE] " + joinPoint);
-    }
-
-    @After("execution(* com.example.demo.service.*.*(..))")
-    public void logAfter(JoinPoint joinPoint) {
-        logger.info("[AFTER] " + joinPoint);
-    }
+//    @Before("execution(* com.example.demo.service.*.*(..))")
+//    public void logBefore(JoinPoint joinPoint) {
+//        logger.info("[BEFORE] " + joinPoint);
+//    }
+//
+//    @After("execution(* com.example.demo.service.*.*(..))")
+//    public void logAfter(JoinPoint joinPoint) {
+//        logger.info("[AFTER] " + joinPoint);
+//    }
 }
